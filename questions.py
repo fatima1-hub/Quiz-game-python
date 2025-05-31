f5c0116ce013af80d9b60544911b73c119a1f26f
Ayesha-kiran1 <ayeshaa6811@gmail.com>
2025-05-31 21:30:49 +0500

    Update questions.py
    File updated

--- questions.py
+++ questions.py
@@ -3,9 +3,9 @@ questions = [
     {"question": "What is the boiling point of water?", "options": ["90°C", "50°C", "100°C", "120°C"], "answer": "100°C"},
     {"question": "Who is the founder of Microsoft?", "options": ["Steve Jobs", "Elon Musk", "Bill Gates", "Mark Zuckerberg"], "answer": "Bill Gates"},
     {"question": "HTML stands for?", "options": ["Hyper Trainer Marking Language", "Hyper Text Markup Language", "High Text Machine Language", "None"], "answer": "Hyper Text Markup Language"},
-    {"question": "What is the largest ocean on Earth?", "options": ["Atlantic", "Indian", "Arctic", "Pacific"], "answer": "Pacific"}
+    {"question": "What is the largest ocean on Earth?", "options": ["Atlantic", "Indian", "Arctic", "Pacific"], "answer": "Pacific"},
     {"question": "What is the capital city of Japan?","options": ["Beijing", "Seoul", "Bangkok", "Tokyo"],"answer": "Tokyo"},
     {"question": "What is the process by which plants make their food?","options": ["Respiration", "Digestion", "Photosynthesis", "Fermentation"],"answer": "Photosynthesis"},
     {"question": "Which element has the chemical symbol 'O'?","options": ["Oxygen", "Gold", "Osmium", "Iron"],"answer": "Oxygen"},
-    {"question": "Who painted the Mona Lisa?","options": ["Vincent Van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Claude Monet"],"answer": "Leonardo da Vinci"},
+    {"question": "Who painted the Mona Lisa?","options": ["Vincent Van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Claude Monet"],"answer": "Leonardo da Vinci"}
 ]
